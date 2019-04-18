1a314ff7f4e42d25002eeb2f48983565f6492a9d
boyacao <bycao96@uw.edu>
2019-04-18 14:42:49 -0700

    refine according to Slide

--- app/src/main/java/edu/washington/bycao96/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/bycao96/activityspy/MainActivity.kt
@@ -5,43 +5,43 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    val Tag = "Activity Spy"
+    val TAG = "Activity Spy"
     /*
 
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i(Tag, "onCreate event fired")
+        Log.i(TAG, "onCreate event fired")
         setContentView(R.layout.activity_main)
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i(Tag, "onCreate event fired");
+        Log.i(TAG, "onCreate event fired");
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.i(Tag, "onRestart event fired")
+        Log.i(TAG, "onRestart event fired")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i(Tag, "onPause event fired")
+        Log.i(TAG, "onPause event fired")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i(Tag, "onStop event fired")
+        Log.i(TAG, "onStop event fired")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i(Tag,"onResume event fired")
+        Log.i(TAG,"onResume event fired")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i(Tag, "We're going down, Captain")
+        Log.i(TAG, "We're going down, Captain")
     }
 }
